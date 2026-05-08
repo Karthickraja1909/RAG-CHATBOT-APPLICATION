45572c55ee78fe8e5ac34bb2c7dd07308fed89ff
king <king@gmail.com>
2026-05-08 17:35:20 +0530

    max token

--- evaluation/ragas_evaluator.py
+++ evaluation/ragas_evaluator.py
@@ -64,6 +64,7 @@ class RagasEvaluator:
                     api_key=self.settings.openrouter_api_key,
                     base_url=self.settings.openrouter_base_url,
                     temperature=0.0,
+                    max_tokens=2048,
                 )
                 embeddings = OpenAIEmbeddings(
                     model=self.settings.eval_embedding_model,
